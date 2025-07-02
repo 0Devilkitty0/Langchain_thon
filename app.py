90dc9f8f77ca7b2d75db9c73cad0098fd16bfbb5
0Devilkitty0 <147805554+0Devilkitty0@users.noreply.github.com>
2025-07-02 22:49:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 import time
 import streamlit as st
-
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
